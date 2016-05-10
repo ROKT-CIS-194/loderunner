e4a9f8fb4ed6fcdb8ad2d0b71cba37be6e54114e
Ben Sinclair <ben.sinclair@rokt.com>
2016-05-10 13:58:47 +1000

    Tweaks

--- src/Main.elm
+++ src/Main.elm
@@ -1,15 +1,14 @@
 module Main where
 
-import Char
+import Char exposing (KeyCode)
 import Graphics.Element exposing (Element, show)
-import Keyboard
+import Keyboard exposing (keysDown)
 import List
 import Set
-import Task exposing (Task)
 
 type Key = Up | Down | Left | Right | DigLeft | DigRight
 
-toKey : Char.KeyCode -> Maybe Key
+toKey : KeyCode -> Maybe Key
 toKey code =
   case Char.toUpper (Char.fromCode code) of
     'T' -> Just DigLeft
@@ -20,14 +19,15 @@ toKey code =
     'J' -> Just Right
     _ -> Nothing
 
-keys : Signal (Maybe Key)
-keys =
+currentKey : Signal (Maybe Key)
+currentKey =
   let oneOnly xs = if List.length xs == 1 then List.head xs else Nothing
-  in Signal.map (Set.toList >> List.filterMap toKey >> oneOnly) Keyboard.keysDown
+  in Signal.map (Set.toList >> List.filterMap toKey >> oneOnly) keysDown
 
 type alias GameState
   = { key : Maybe Key
-    , position : Int }
+    , position : Int
+    }
 
 main : Signal Element
 main =
@@ -42,4 +42,4 @@ main =
                     _ -> st.position
         in { st | key = k, position = pos }
 
-  in keys |> Signal.foldp step state0 |> Signal.map show
+  in currentKey |> Signal.foldp step state0 |> Signal.map show
