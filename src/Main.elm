6e703a8c80429b50396521ab60de06e227c097d6
Ben Sinclair <ben.d.sinclair@gmail.com>
2016-05-10 18:31:23 +1000

    Add fps ticker events

--- src/Main.elm
+++ src/Main.elm
@@ -5,6 +5,7 @@ import Graphics.Element exposing (Element, show)
 import Keyboard exposing (keysDown)
 import List
 import Set
+import Time exposing (Time)
 
 type Key = Up | Down | Left | Right | DigLeft | DigRight
 
@@ -19,27 +20,38 @@ toKey code =
     'J' -> Just Right
     _ -> Nothing
 
-currentKey : Signal (Maybe Key)
-currentKey =
-  let oneOnly xs = if List.length xs == 1 then List.head xs else Nothing
-  in Signal.map (Set.toList >> List.filterMap toKey >> oneOnly) keysDown
+type Input = InputKey (Maybe Key) | InputTime Time
+
+inputs : Signal Input
+inputs =
+  let currentKey = Signal.map (Set.toList >> List.filterMap toKey >> oneOnly) keysDown
+      oneOnly xs = if List.length xs == 1 then List.head xs else Nothing
+  in Signal.mergeMany [ Signal.map InputKey currentKey
+                      , Signal.map InputTime (Time.fps 15)
+                      ]
 
 type alias GameState
-  = { key : Maybe Key
+  = { key      : Maybe Key
     , position : Int
     }
 
 main : Signal Element
 main =
-  let state0 : GameState
-      state0 = { key = Nothing, position = 0 }
+  let initialState : GameState
+      initialState = { key = Nothing, position = 0 }
 
-      step : Maybe Key -> GameState -> GameState
-      step k st =
-        let pos = case k of
-                    Just Left -> st.position - 1
-                    Just Right -> st.position + 1
-                    _ -> st.position
-        in { st | key = k, position = pos }
+      view : GameState -> Element
+      view st = show { position = st.position }
 
-  in currentKey |> Signal.foldp step state0 |> Signal.map show
+      step : Input -> GameState -> GameState
+      step inp st = case inp of
+        InputKey k ->
+          { st | key = k }
+        InputTime t ->
+          let pos = case st.key of
+                      Just Left  -> st.position - 1
+                      Just Right -> st.position + 1
+                      _          -> st.position
+          in { st | position = pos }
+
+  in inputs |> Signal.foldp step initialState |> Signal.map view
